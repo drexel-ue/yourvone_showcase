d28c623b4a3f272e5127835da4aa11aa50a70349
ikesh pack <packappsforyou@gmail.com>
2019-10-31 19:02:43 -0400

    Code optimization

--- lib/views/landing.dart
+++ lib/views/landing.dart
@@ -7,32 +7,28 @@ class Landing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedContainer(
-          duration: Duration(seconds: 2),
-          curve: Curves.easeInOut,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/logo.png',
-                height: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/logo.png',
+              height: 150,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'YourVOne Showcase',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 40),
+            FlatButton(
+              onPressed: () {},
+              color: Colors.teal,
+              child: const Text(
+                'ENTER',
+                style: const TextStyle(fontSize: 16),
               ),
-              SizedBox(height: 10),
-              Text(
-                'YourVOne Showcase',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: 40),
-              FlatButton(
-                onPressed: () {},
-                color: Colors.teal,
-                child: Text(
-                  'ENTER',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
