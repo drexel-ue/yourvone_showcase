c3bcdc0ce27ed9532c2ea2b9e70128ff3c59d529
ikesh pack <packappsforyou@gmail.com>
2019-11-01 18:53:53 -0400

    Setup basic user bloc

---
+++ lib/blocs/user_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:rxdart/subjects.dart';
+import 'package:yourvone_showcase/auth_services/user.dart';
+import 'package:yourvone_showcase/events/user_event.dart';
+
+class UserBloc {
+  User user;
+
+  final _userSubject = BehaviorSubject<User>();
+  Stream<User> get userStream => _userSubject.stream;
+
+  final eventSubject = BehaviorSubject<UserEvent>();
+
+  UserBloc() {
+    eventSubject.listen((UserEvent event) {});
+  }
+
+  void dispose() {
+    _userSubject?.drain();
+    eventSubject?.drain();
+  }
+}
