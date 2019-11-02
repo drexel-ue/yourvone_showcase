7f00c7cb5ee50b7e816d40ad0327b21a8429d7fc
ikesh pack <packappsforyou@gmail.com>
2019-11-02 08:39:28 -0400

    Add signout button

--- lib/views/nexus.dart
+++ lib/views/nexus.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yourvone_showcase/auth_services/firebase_auth_service.dart';
 import 'package:yourvone_showcase/utils.dart';
 import 'package:yourvone_showcase/widgets/nexus/navigation_tile.dart';
 
 class Nexus extends StatelessWidget {
-  const Nexus({Key key}) : super(key: key);
+  Nexus({Key key}) : super(key: key);
+
+  final _auth = FirebaseAuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +17,19 @@ class Nexus extends StatelessWidget {
           child: Image.asset('assets/full_logo.png'),
         ),
         title: const Text('Widgets'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              _auth.signOut();
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                authRoute,
+                (Route route) => false,
+              );
+            },
+            child: const Text('SIGN OUT'),
+          ),
+        ],
       ),
       body: ListView(
         children: <Widget>[
