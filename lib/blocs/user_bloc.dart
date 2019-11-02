2744169077c88c73ae60a9f3043f11da17aea987
ikesh pack <packappsforyou@gmail.com>
2019-11-02 08:26:33 -0400

    Add logic in userbloc to handle setuser event

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -13,7 +13,14 @@ class UserBloc {
   final eventSubject = BehaviorSubject<UserEvent>();
 
   UserBloc() {
-    eventSubject.listen((UserEvent event) {});
+    eventSubject.listen((UserEvent event) {
+      if (event is SetUser) _setUser(event.user);
+    });
+  }
+
+  void _setUser(User _user) {
+    user = _user;
+    _userSubject.add(user);
   }
 
   void dispose() {
