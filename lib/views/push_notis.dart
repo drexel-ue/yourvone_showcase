c057819d5be18495ece94ddb8507af877ed14c19
ikesh pack <packappsforyou@gmail.com>
2019-11-02 08:45:37 -0400

    Begin push notification view

---
+++ lib/views/push_notis.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PushNotis extends StatelessWidget {
+  const PushNotis({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Hero(
+          tag: 'logo',
+          child: Image.asset('assets/full_logo.png'),
+        ),
+        title: const Text('Push Notifications'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.notifications_off),
+              title: Text('Enable Push Notifications'),
+              trailing: Switch.adaptive(
+                value: false,
+                onChanged: (bool value) {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
