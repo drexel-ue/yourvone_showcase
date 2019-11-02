4741387d3c707cfb4038e5aff6ac70f472ae1899
ikesh pack <packappsforyou@gmail.com>
2019-11-02 15:02:38 -0400

    Setup toggling of push notification permissions

--- lib/views/push_notis.dart
+++ lib/views/push_notis.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yourvone_showcase/blocs/notification_bloc.dart';
+import 'package:yourvone_showcase/events/notification_event.dart';
 
 class PushNotis extends StatelessWidget {
   const PushNotis({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final notiBloc = Provider.of<NotificationBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         leading: Hero(
@@ -17,13 +22,25 @@ class PushNotis extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ListTile(
-              leading: const Icon(Icons.notifications_off),
-              title: const Text('Enable Push Notifications'),
-              trailing: Switch.adaptive(
-                value: false,
-                onChanged: (bool value) {},
-              ),
+            StreamBuilder<String>(
+              stream: notiBloc.permissionStream,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                return ListTile(
+                  leading: Icon(snapshot.hasData
+                      ? Icons.notifications_active
+                      : Icons.notifications_off),
+                  title: Text(snapshot.hasData
+                      ? 'Disable Push Notifications'
+                      : 'Enable Push Notifications'),
+                  trailing: Switch.adaptive(
+                    value: snapshot.hasData,
+                    onChanged: (bool value) {
+                      notiBloc.eventSubject.add(
+                          snapshot.hasData ? RevokePermission() : Initialize());
+                    },
+                  ),
+                );
+              },
             ),
           ],
         ),
